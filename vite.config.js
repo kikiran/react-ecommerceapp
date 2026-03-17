55bb4caab94253e319e68bdb14d52edd93908cce
imandi.kumar <imandi.kumar@upbound.com>
2026-03-17 16:02:27 +0530

    worked on config file changes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
+	define: {
+		"import.meta.env.VITE_APP_URL": JSON.stringify(
+			process.env.VITE_APP_URL,
+		),
+	},
 });
